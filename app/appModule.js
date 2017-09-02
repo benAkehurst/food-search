a78c657d99acef2bc20aa97547b7ac5d53425a51
mistabenjo <benakehurst@hotmail.com>
2017-09-02 12:18:12 +0300

    Revert "changed formatting for app module"
    This reverts commit 7126ac27a9a11d1f7c8bd9b945cc053cef66a3e1.

--- app/appModule.js
+++ app/appModule.js
@@ -2,49 +2,60 @@
     
     'use strict';
     
-    var appModule = angular.module("app", ["ngRoute","ngMaterial","loginModule","routesModule", "exploreModule","profileModule"])
+    var appModule = angular.module("appModule", ["ngRoute",
+                                                 "ngMaterial",
+                                                 "loginModule",
+                                                 "routesModule",
+                                                 "exploreModule",
+                                                 "profileModule"
+                                                ]);
     
-      appModule.config(function($routeProvider){
+    appModule.config(function($routeProvider){
         
         $routeProvider
-        .when("/",{
-            controller:"LoginController",
-            templateUrl:"app/loginModule/loginView.html"
-        })
+        
         .when("/login",{
             controller:"LoginController",
             templateUrl:"app/loginModule/loginView.html"
         })
+        
         .when("/routes",{
             controller:"RoutesController",
             templateUrl:"app/routesModule/routesView.html"
         })
+
         .when("/routes/saveRoute",{
             controller:"RoutesController",
             templateUrl:"app/routesModule/routesView.html"
         })
-        .when("/routes/getAllRoutes",{
+
+        .when("/routes/getallRoutes",{
             controller:"RoutesController",
             templateUrl:"app/routesModule/routesView.html"
         })
+
         .when("/routes/editRoute/:_id",{
             controller:"RoutesController",
             templateUrl:"app/routesModule/routesView.html"
         })
+
         .when("/routes/deleteRoute/:_id",{
             controller:"RoutesController",
             templateUrl:"app/routesModule/routesView.html"
         })
+
         .when("/explore",{
             controller:"ExploreController",
             templateUrl:"app/exploreModule/exploreView.html"
         })
+
         .when("/profile",{
             controller:"ProfileController",
             templateUrl:"app/profileModule/profileView.html"
         })
+        
         .otherwise({
-			redirectTo:"/"
+			redirectTo:"/routes"
 		});
         
     });
