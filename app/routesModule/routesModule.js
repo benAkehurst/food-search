065b589213e50c17d382690b061f524483e5de46
mistabenjo <benakehurst@hotmail.com>
2017-09-13 23:55:11 +0300

    method to get user location and method to get results from google places

--- app/routesModule/routesModule.js
+++ app/routesModule/routesModule.js
@@ -5,29 +5,16 @@
     var routesModule = angular.module("routesModule", []);
 
     routesModule.controller("RoutesController", function($q, $http, $scope, $rootScope, $location) {
-        
-        var userLoc = function(pos) {
-            var latitude = pos.latitude;
-            var longitude = pos.longitude;
-            places(latitude, longitude);
-        }
 
-        var places = function(latitude, longitude) {
-            var latitude = latitude;
-            var longitude = longitude;
-            var base = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
-            var longLat = latitude + "," + longitude;
-            var radiusType = "&radius=2000&type=restaurant";
-            var key = "&key=AIzaSyD32rWtceO4-3aY02cxmsYwihYuNEWVIOw";
-            var serachTerm = base + longLat + radiusType + key;
-
+        var userCity = function() {
             $http({
-                method: "GET",
-                url: serachTerm
-            }).then(function success(response) {
-                $scope.locationResults = response.data;
-            }, function error(response) {
-                console.log(response.statusText);
+                method: 'GET',
+                url: 'https://ipinfo.io'
+            }).then(function successCallback(response) {
+                $scope.city = response.data.city;
+                // var loc = response.data.loc;
+            }, function errorCallback(response) {
+                console.log(response);
             });
         }
 
@@ -47,7 +34,41 @@
                     userLoc(pos);
                 });
         }
+
+        var userLoc = function(pos) {
+            var latitude = pos.latitude;
+            var longitude = pos.longitude;
+            var locObj = {
+                latitude: latitude,
+                longitude: longitude
+            }
+            makeSearch(locObj);
+        }
+
+        var makeSearch = function(locObj) {
+            var base = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
+            var longLat = locObj.latitude + "," + locObj.longitude;
+            var radius = "&radius=2500"
+            var type = "&type=restaurant";
+            var key = "&key=AIzaSyD32rWtceO4-3aY02cxmsYwihYuNEWVIOw";
+            var serachTerm = base + longLat + radius + type + key;
+            places(serachTerm);
+        }
+
+        var places = function(serachTerm) {
+            $http({
+                method: "GET",
+                url: serachTerm
+            }).then(function success(response) {
+                console.log(response.data);
+            }, function error(response) {
+                console.log(response.statusText);
+            });
+        }
+
         getUserLocation();
+        userCity();
 
     });
+
 })();
