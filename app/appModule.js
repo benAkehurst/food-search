e947927cb7a2dfcc72db487b2cb59f431faf4e6b
mistabenjo <benakehurst@hotmail.com>
2017-09-15 11:59:28 +0300

    added angularjs materials theme

--- app/appModule.js
+++ app/appModule.js
@@ -1,63 +1,69 @@
-(function(){
-    
+(function() {
+
     'use strict';
-    
+
     var appModule = angular.module("appModule", ["ngRoute",
-                                                 "ngMaterial",
-                                                 "loginModule",
-                                                 "routesModule",
-                                                 "exploreModule",
-                                                 "profileModule"
-                                                ]);
-    
-    appModule.config(function($routeProvider){
-        
-        $routeProvider
-        
-        .when("/login",{
-            controller:"LoginController",
-            templateUrl:"app/loginModule/loginView.html"
-        })
-        
-        .when("/routes",{
-            controller:"RoutesController",
-            templateUrl:"app/routesModule/routesView.html"
+        "ngMaterial",
+        "loginModule",
+        "routesModule",
+        "exploreModule",
+        "profileModule"
+    ]);
+
+    appModule.config(function($routeProvider) {
+
+            $routeProvider
+
+                .when("/login", {
+                    controller: "LoginController",
+                    templateUrl: "app/loginModule/loginView.html"
+                })
+
+                .when("/routes", {
+                    controller: "RoutesController",
+                    templateUrl: "app/routesModule/routesView.html"
+                })
+
+                .when("/routes/saveRoute", {
+                    controller: "RoutesController",
+                    templateUrl: "app/routesModule/routesView.html"
+                })
+
+                .when("/routes/getallRoutes", {
+                    controller: "RoutesController",
+                    templateUrl: "app/routesModule/routesView.html"
+                })
+
+                .when("/routes/editRoute/:_id", {
+                    controller: "RoutesController",
+                    templateUrl: "app/routesModule/routesView.html"
+                })
+
+                .when("/routes/deleteRoute/:_id", {
+                    controller: "RoutesController",
+                    templateUrl: "app/routesModule/routesView.html"
+                })
+
+                .when("/explore", {
+                    controller: "ExploreController",
+                    templateUrl: "app/exploreModule/exploreView.html"
+                })
+
+                .when("/profile", {
+                    controller: "ProfileController",
+                    templateUrl: "app/profileModule/profileView.html"
+                })
+
+                .otherwise({
+                    redirectTo: "/routes"
+                });
+
         })
 
-        .when("/routes/saveRoute",{
-            controller:"RoutesController",
-            templateUrl:"app/routesModule/routesView.html"
-        })
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue')
+                .accentPalette('orange');
+        });
 
-        .when("/routes/getallRoutes",{
-            controller:"RoutesController",
-            templateUrl:"app/routesModule/routesView.html"
-        })
-
-        .when("/routes/editRoute/:_id",{
-            controller:"RoutesController",
-            templateUrl:"app/routesModule/routesView.html"
-        })
-
-        .when("/routes/deleteRoute/:_id",{
-            controller:"RoutesController",
-            templateUrl:"app/routesModule/routesView.html"
-        })
-
-        .when("/explore",{
-            controller:"ExploreController",
-            templateUrl:"app/exploreModule/exploreView.html"
-        })
-
-        .when("/profile",{
-            controller:"ProfileController",
-            templateUrl:"app/profileModule/profileView.html"
-        })
-        
-        .otherwise({
-			redirectTo:"/routes"
-		});
-        
-    });
-    
 })();
