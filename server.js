a7c6d51c87b0d9fcfaecb629aa0e9f950bf89dda
mistabenjo <benakehurst@hotmail.com>
2017-09-17 20:11:28 +0300

    tinkering to try and do http req on server

--- server.js
+++ server.js
@@ -7,42 +7,37 @@ var http = require("http");
 var https = require("https");
 
 // Uses for getting page elements
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
 // Connect to DB with mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/MunchDB", function(err){
-  if(err){
-    console.log("Error: " + err);
-  }
-  else {
-    console.log("Connected to Database")
-  }
+mongoose.connect("mongodb://localhost:27017/MunchDB", function(err) {
+    if (err) {
+        console.log("Error: " + err);
+    } else {
+        console.log("Connected to Database")
+    }
 });
 
 // Mongoose Schema - New Candy
-var Route = mongoose.model("Route",{
-  city:String,
-  cusineOne:String,
-  cusineTwo:String,
-  cusineThree:String,
-  locationOne:Object,
-  locationTwo:Object,
-  locationThree:Object
+var Route = mongoose.model("Route", {
+    city: String,
+    cusineOne: String,
+    cusineTwo: String,
+    cusineThree: String,
+    locationOne: Object,
+    locationTwo: Object,
+    locationThree: Object
 });
 
-var Cuisine = mongoose.model("Cusine",{
-  type:String
+var Cuisine = mongoose.model("Cusine", {
+    type: String
 });
-
-// Here I need to write the http request to get the locations server side - https://stackoverflow.com/questions/9577611/http-get-request-in-node-js-express
-// Need from the user thier location
-// send back json with results
 
 // CREATE - POST candy to DB
-app.post("/saveRoute",function(request, response){
+app.post("/saveRoute", function(request, response) {
     var newRoute = new Route(request.body);
     newRoute.save();
     newRoute.status(201);
@@ -50,49 +45,45 @@ app.post("/saveRoute",function(request, response){
 });
 
 // READ - GET all candy from DB
-app.get("/getallRoutes", function(request, response){
-   Route.find({}).exec(function(err,route){
-       if(err){
-           console.log("Error" + " " + err);
-       }
-       else{
-           response.send(route);
-           console.log("Routes Loaded and sent back to FE");
-       }
-   }) 
+app.get("/getallRoutes", function(request, response) {
+    Route.find({}).exec(function(err, route) {
+        if (err) {
+            console.log("Error" + " " + err);
+        } else {
+            response.send(route);
+            console.log("Routes Loaded and sent back to FE");
+        }
+    })
 });
 
 // UPDATE - PUT (update) a candy in the DB by the id
-app.put("/editRoute/:_id", function(request, response){
-    
+app.put("/editRoute/:_id", function(request, response) {
+
     var editRoute = request.body;
 
-    Route.findOne({_id:request.params._id}).exec(function(err,singleRoute){
-        if(err){
+    Route.findOne({ _id: request.params._id }).exec(function(err, singleRoute) {
+        if (err) {
             console.log("Error" + " " + err);
-        }
-        else{
-            if(singleRoute){
+        } else {
+            if (singleRoute) {
                 singleRoute.locationOne = request.body.newLocationOne;
                 singleRoute.locationTwo = request.body.newLocationTwo;
                 singleRoute.locationThree = request.body.newLocationThree;
                 singleRoute.save();
                 response.send(singleRoute);
-            }
-            else{
+            } else {
                 console.log("There was an issue editing the route. The error was: " + " " + err.status);
-            }      
+            }
         }
     })
 });
 
 // DELETE - delete specific candy from the DB
-app.delete("/deleteRoute/:_id",function(request,response){
-    Route.remove({_id:request.params._id}).exec(function(err,deletedRoute){
-        if(err){
+app.delete("/deleteRoute/:_id", function(request, response) {
+    Route.remove({ _id: request.params._id }).exec(function(err, deletedRoute) {
+        if (err) {
             console.log("Error" + " " + err)
-        }
-        else{
+        } else {
             response.status(204);
             response.send("Route has been removed from the DB");
         }
@@ -100,6 +91,6 @@ app.delete("/deleteRoute/:_id",function(request,response){
 });
 
 // allows the server to connect on port 3000 
-app.listen(3000,function(){
-  console.log("Listening on http://localhost:3000");
+app.listen(3000, function() {
+    console.log("Listening on http://localhost:3000");
 });
