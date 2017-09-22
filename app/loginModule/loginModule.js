df48d562adff9cc014705fed706edc7026ac2659
mistabenjo <benakehurst@hotmail.com>
2017-09-22 17:40:38 +0300

    basic markup for login/sign up

--- app/loginModule/loginModule.js
+++ app/loginModule/loginModule.js
@@ -1,12 +1,22 @@
 (function() {
 
     'use strict';
-    
+
     var loginModule = angular.module("loginModule", []);
 
-    loginModule.controller("LoginController", function($http, $scope, $rootScope, $location) {
+    loginModule.controller("LoginController", function($http, $route, $scope, $rootScope, $location) {
 
-        
+    	$scope.login = function(email,password){
+    		console.log(email + "," + password);
+    	}
+
+    	$scope.signUp = function(name,email,password){
+    		console.log(name + "," + email + "," + password);
+    	}
+
+    	$scope.redirectToRoutes = function(){
+    		$location.path('/routes');
+    	}
 
     });
 
