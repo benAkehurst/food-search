64ee4dcc63ed38fd002c26c7b131a8effc49dc10
mistabenjo <benakehurst@hotmail.com>
2017-09-23 19:17:49 +0300

    basic profile build

--- app/profileModule/profileModule.js
+++ app/profileModule/profileModule.js
@@ -6,8 +6,23 @@
 
     profileModule.controller("ProfileController", function($http, $scope, $rootScope, $location) {
 
-        
+        var userDetail = function(){
+        	var userDetails = getSessionItems();
+        	$scope.userDetail = userDetails;
+        }
+
+        userDetail();
+
+        $scope.logout = function() {
+            sessionStorage.clear();
+            $location.path('/login');
+        }
 
     });
+
+    function getSessionItems() {
+        var sessionEmail = sessionStorage.getItem("email");
+        return sessionEmail;
+    }
 
 })();
