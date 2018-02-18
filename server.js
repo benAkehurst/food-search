1d821262ea35fc335465fbb8d0e76af4203e2363
benAkehurst <benakehurst@hotmail.com>
2018-02-18 19:25:06 +0200

    contiunue
    messing with implimenting angular2... it didn't work

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
-// Sets default cors to allow requests from this server
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
